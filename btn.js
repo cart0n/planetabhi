91d50d02e37108cd2aaca7142991360e6ed1bbe1
cart0n <cart0n.zlskv@gmail.com>
2020-01-23 18:32:01 +0200

    1.0

---
+++ btn.js
@@ -0,0 +1,48 @@
+	function readMore1(){
+		let metus1 = document.getElementById("metus1");
+		let show1 = document.getElementById("show1");
+		let btn1 = document.getElementById("btn1");
+
+		if  (metus1.style.display ==="none"){
+			metus1.style.display="inline";
+			btn1.innerHTML="Read More..."
+			show1.style.display="none";
+		}
+		else{
+			metus1.style.display="none";
+			btn1.innerHTML="Hide"
+			show1.style.display="inline";
+		}
+	}
+	function readMore2(){
+		let metus2 = document.getElementById("metus2");
+		let show2 = document.getElementById("show2");
+		let btn2 = document.getElementById("btn2");
+
+		if  (metus2.style.display ==="none"){
+			metus2.style.display="inline";
+			btn2.innerHTML="Read More..."
+			show2.style.display="none";
+		}
+		else{
+			metus2.style.display="none";
+			btn2.innerHTML="Hide"
+			show2.style.display="inline";
+		}
+	}
+	function readMore3(){
+		let metus3 = document.getElementById("metus3");
+		let show3 = document.getElementById("show3");
+		let btn3 = document.getElementById("btn3");
+
+		if  (metus3.style.display ==="none"){
+			metus3.style.display="inline";
+			btn3.innerHTML="Read More..."
+			show3.style.display="none";
+		}
+		else{
+			metus3.style.display="none";
+			btn3.innerHTML="Hide"
+			show3.style.display="inline";
+		}
+	}
